7ca25530b698cce37575fd77c1bc015bbef5d8d4
GuiRodriguero <50684900+GuiRodriguero@users.noreply.github.com>
2020-04-07 19:21:32 -0300

    Corrigido!

--- Desafios/Desvio1.py
+++ Desafios/Desvio1.py
@@ -1,8 +1,10 @@
-# PU - PREÇO UNITÁRIO | PO - PAÍS DE ORIGEM | MT - MEIO DE TRANSPORTE | CP - CARGA PERIGOSA
+# PU - PREÇO UNITÁRIO | PO - PAÍS DE ORIGEM | MT - MEIO DE TRANSPORTE | CP - CARGA PERIGOSA | VT = Valor Transporte
 pu = int(input("Digite o preço unitário: "))
 po = input("Digite o país de origem: (1-Brasil, 2-México,3-Outros) ")
 mt = input("Digite o meio de transporte: (T-Terrestre, F-Fluvial, A-Aéreo) ")
 cp = input("É carga perigosa? S/N ")
+vt = 0
+seguro = 0
 
 mt.upper()
 cp.upper()
@@ -14,15 +16,16 @@ if pu > 0 and pu <=100:
 else:
     juros = 0.2
     pu = (pu*juros) + pu
+    print(pu)
 
-if cp == "S":
+if cp == "S" or cp == "s":
     if po == 1:
         vt = 21
     if po == 2:
         vt = 27
     if po == 3:
         vt = 50
-elif cp == "N":
+elif cp == "N" or cp == "n":
     if po == 1:
         vt = 21
     if po == 2:
@@ -30,11 +33,10 @@ elif cp == "N":
     if po == 3:
         vt = 40
 
-if po == 2 and mt == "A":
+if po == 2 and mt == "A" or mt == "a":
     seguro = pu/2
 else:
-    pu = 0
+    seguro = 0
 
 pf = pu + juros + vt + seguro
-
-print("Valor total: " + pf)
+print("Valor total: ", pf)
